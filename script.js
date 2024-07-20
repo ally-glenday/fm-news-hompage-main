01183e46e968d994899ec42392277159c2572b25
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2024-07-20 11:54:48 +0900

    Hamburger working

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /* Function that toggles navigation hamburger menu */ 
 function toggleMenu(){
     document.getElementById("hamburger-menu").classList.toggle("display-block");  
-    document.getElementById("hamburger-button").classList.toggle("close-menu");  
+    document.getElementById("hamburger-button").classList.toggle("close-menu"); 
+    /* For later, try to change the background to a more grey colour when the pop up menu appears */ 
+    // document.body.style.backgroundColor = "#81808a"; 
+    
 }
