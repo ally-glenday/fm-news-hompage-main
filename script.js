a325b063fa92bd272db9cee518b4f3539d82396a
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2024-07-20 09:12:48 +0900

    Change nav button to img

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /* Function that toggles navigation hamburger menu */ 
 function toggleMenu(){
     document.getElementById("hamburger-menu").classList.toggle("display-block");  
+    document.getElementById("").classList.toggle("display-none");  
 }
