dbf45bad1009e2bac87ff421937d962f7ffeddc2
ally-glenday <66145089+ally-glenday@users.noreply.github.com>
2024-08-01 19:34:18 +0900

    ほとんど終わった

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function toggleMenu(){
     document.getElementById("hamburger-menu").classList.toggle("display-block");  
     document.getElementById("hamburger-button").classList.toggle("close-menu"); 
     /* For later, try to change the background to a more grey colour when the pop up menu appears */ 
+   
     // document.body.style.backgroundColor = "#81808a"; 
     
 }
